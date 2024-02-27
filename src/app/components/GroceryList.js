969255eb6d243ca7c0dc6c4cf6db7c4f14559129
Dmytro <ccccuxoy@gmail.com>
2024-02-27 19:32:59 +0200

    main logic added, without styles

--- src/app/components/GroceryList.js
+++ src/app/components/GroceryList.js
@@ -1,46 +1,36 @@
-"use client";
-
 import React, { useState } from "react";
 import { useQuery, useMutation, queryCache, useQueryClient } from "react-query";
-
-const fetchItems = async () => {
-  const response = await fetch("http://localhost:3001/items");
-  return response.json();
-};
-
-const addItem = async (itemName) => {
-  const response = await fetch("http://localhost:3001/items", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: itemName, isBought: false }),
-  });
-  return response.json();
-};
-
-const toggleIsBought = async (itemId, isBought) => {
-  const response = await fetch(`http://localhost:3001/items/${itemId}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ isBought: !isBought }),
-  });
-  return response.json();
-};
-
-const deleteItem = async (itemId) => {
-  const response = await fetch(`http://localhost:3001/items/${itemId}`, {
-    method: "DELETE",
-  });
-  return response.json();
-};
+import {
+  fetchItems,
+  addItem,
+  toggleIsBought,
+  deleteItem,
+  editItem,
+} from "../../services/services.js";
 
 const GroceryList = () => {
   const queryClient = useQueryClient();
   const [text, setText] = useState("");
+  const [price, setPrice] = useState(0);
+  const [isBought, setIsBought] = useState(false);
+
+  const [modalText, setModalText] = useState("");
+  const [modalPrice, setModalPrice] = useState(0);
+  const [modalIsBought, setModalIsBought] = useState(false);
+
+  const [selectedItemId, setSelectedItemId] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const { data: items, status, error } = useQuery("items", fetchItems);
+
+  const sum = items
+    ? items
+        .filter((item) => item.isBought)
+        .reduce((acc, item) => acc + item.price, 0)
+    : 0;
+
+  const potenialSum = items
+    ? items.reduce((acc, item) => acc + item.price, 0)
+    : 0;
 
   const addItemMutation = useMutation({
     mutationFn: addItem,
@@ -63,23 +53,74 @@ const GroceryList = () => {
     },
   });
 
+  const editItemMutation = useMutation({
+    mutationFn: editItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["items"] });
+    },
+  });
+
   const handleInputChange = (e) => {
     setText(e.target.value);
   };
 
+  const handleModalInputChange = (e) => {
+    setModalText(e.target.value);
+  };
+
   const handleAddItem = () => {
     if (text) {
-      addItemMutation.mutate(text);
+      addItemMutation.mutate({ itemName: text, itemPrice: price });
       setText("");
+      setPrice(0);
     }
   };
 
-  const handleToggleIsBought = (id, isBought) => {
-    toggleIsBoughtMutation.mutate(id, isBought);
+  const handleToggleIsBought = (id) => {
+    setIsBought(!isBought);
+    toggleIsBoughtMutation.mutate({ id: id, isBought: isBought });
   };
 
   const handleDeleteItem = (id) => {
     deleteItemMutation.mutate(id);
+  };
+
+  const handleOpenPopup = (id) => {
+    setSelectedItemId(id);
+    const selectedItem = items.filter((item) => item.id === id);
+
+    setModalText(selectedItem[0].name);
+    setModalPrice(selectedItem[0].price);
+    setModalIsBought(selectedItem[0].isBought);
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+    setSelectedItemId(null);
+    setModalText("");
+  };
+
+  const handleEditItem = () => {
+    if (modalText) {
+      editItemMutation.mutate({
+        id: selectedItemId,
+        isBought: modalIsBought,
+        name: modalText,
+        price: modalPrice,
+      });
+    }
+
+    setIsPopupOpen(false);
+    setModalText("");
+    setModalPrice(0);
+  };
+
+  const handleDeleteSelectedItem = () => {
+    if (selectedItemId) {
+      deleteItemMutation.mutate(selectedItemId);
+      setIsPopupOpen(false);
+    }
   };
 
   if (status === "loading") return <div>Loading...</div>;
@@ -88,6 +129,12 @@ const GroceryList = () => {
   return (
     <div>
       <input type="text" value={text} onChange={handleInputChange} />
+      <input
+        type="number"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
       <button onClick={handleAddItem}> Add Item </button>
       <ul>
         {items.map((item) => (
@@ -97,16 +144,42 @@ const GroceryList = () => {
               textDecoration: item.isBought ? "line-through" : "none",
             }}
           >
-            {item.name}
+            {item.name} {item.price && `${item.price} $`}
             <button
               onClick={() => handleToggleIsBought(item.id, item.isBought)}
             >
               {item.isBought ? "Mark Incomplete" : "Mark Complete"}
             </button>
             <button onClick={() => handleDeleteItem(item.id)}> Delete </button>
+            <button onClick={() => handleOpenPopup(item.id)}>Edit</button>
           </li>
         ))}
       </ul>
+      <div>{`sum: ${sum} $`}</div>
+      <div>{`potencial sum: ${potenialSum} $`}</div>
+
+      {isPopupOpen && (
+        <div className="popup">
+          <input
+            type="text"
+            value={modalText}
+            onChange={handleModalInputChange}
+          />
+          <input
+            type="number"
+            value={modalPrice}
+            onChange={(e) => setModalPrice(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            checked={modalIsBought}
+            onChange={(e) => setModalIsBought(e.target.checked)}
+          />
+          <button onClick={handleEditItem}>Save</button>
+          <button onClick={handleDeleteSelectedItem}>Delete</button>
+          <button onClick={handleClosePopup}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/services/services.js
@@ -0,0 +1,48 @@
+export const fetchItems = async () => {
+  const response = await fetch("http://localhost:3001/items");
+  return response.json();
+};
+
+export const addItem = async ({ itemName, itemPrice }) => {
+  const response = await fetch("http://localhost:3001/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: itemName,
+      isBought: false,
+      price: +itemPrice,
+    }),
+  });
+  return response.json();
+};
+
+export const toggleIsBought = async ({ id, isBought }) => {
+  const response = await fetch(`http://localhost:3001/items/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ isBought: isBought }),
+  });
+  return response.json();
+};
+
+export const editItem = async ({ id, isBought, name, price }) => {
+  const response = await fetch(`http://localhost:3001/items/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ isBought: isBought, name, price: +price }),
+  });
+  return response.json();
+};
+
+export const deleteItem = async (itemId) => {
+  const response = await fetch(`http://localhost:3001/items/${itemId}`, {
+    method: "DELETE",
+  });
+  return response.json();
+};
