be85ffe10a65daf48238c57385f26031433f7766
Dmytro <ccccuxoy@gmail.com>
2024-02-28 18:09:38 +0200

    services fixed, due to new server

--- src/services/services.js
+++ src/services/services.js
@@ -1,48 +1,62 @@
 export const fetchItems = async () => {
-  const response = await fetch("http://localhost:3001/items");
+  const response = await fetch(
+    "https://grocery-server-100500-002e71001da6.herokuapp.com/items"
+  );
   return response.json();
 };
 
 export const addItem = async ({ itemName, itemPrice }) => {
-  const response = await fetch("http://localhost:3001/items", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: itemName,
-      isBought: false,
-      price: +itemPrice,
-    }),
-  });
+  const response = await fetch(
+    "https://grocery-server-100500-002e71001da6.herokuapp.com/items",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: itemName,
+        isBought: false,
+        price: +itemPrice,
+      }),
+    }
+  );
   return response.json();
 };
 
 export const toggleIsBought = async ({ id, isBought }) => {
-  const response = await fetch(`http://localhost:3001/items/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ isBought }),
-  });
+  const response = await fetch(
+    `https://grocery-server-100500-002e71001da6.herokuapp.com/items/${id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isBought }),
+    }
+  );
   return response.json();
 };
 
 export const editItem = async ({ id, isBought, name, price }) => {
-  const response = await fetch(`http://localhost:3001/items/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ isBought: isBought, name, price: +price }),
-  });
+  const response = await fetch(
+    `https://grocery-server-100500-002e71001da6.herokuapp.com/items/${id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isBought: isBought, name, price: +price }),
+    }
+  );
   return response.json();
 };
 
 export const deleteItem = async (itemId) => {
-  const response = await fetch(`http://localhost:3001/items/${itemId}`, {
-    method: "DELETE",
-  });
+  const response = await fetch(
+    `https://grocery-server-100500-002e71001da6.herokuapp.com/items/${itemId}`,
+    {
+      method: "DELETE",
+    }
+  );
   return response.json();
 };
